a016a3d7a4091400ab8aa3919cc1ad2a2904d224
Peter Ondy <90659432+peterondy@users.noreply.github.com>
2022-12-12 07:14:41 +0100

    first commit
    first commit

---
+++ homescript.js
@@ -0,0 +1,54 @@
+/*Start Home Page Script*/
+
+    fetch("./json/login.json")
+    .then((response) => {
+    return response.json();
+    })
+    .then( (login)=> {
+            if(document.location.search == `?user=${login.login.osama.username}`){
+                //Go to home page with user is Osama
+                const tbody = document.getElementById("tbody");
+                fetch("./json/sessions.json")
+                .then((response) => {
+                return response.json();
+                })
+                .then((sessions)=>{
+                    if(sessions.sessions.length > 0){
+                        //alert(sessions.sessions.length)
+                        let time = 0;
+                        for (let index = 0; index < sessions.sessions.length; index++) {
+                            let i = index;
+                            time += parseFloat(sessions.sessions[index].time);
+                            tbody.innerHTML += `
+                                <tr>
+                                    <th scope="row">${i+1}</th>
+                                    <td>${sessions.sessions[index].date}</td>
+                                    <td>${sessions.sessions[index].time} Hours</td>
+                                    <td>${sessions.sessions[index].name}</td>
+                                </tr>
+                            `;
+                        }
+                        document.getElementById("first").innerText = time * 3 + " €";
+                        document.getElementById("last").innerText = time + " Hours";
+                    }else{
+                        console.log('No Data Found');
+                        tbody.innerHTML += `
+                                <tr>
+                                    <h1 class="text-center p-3">No Data Found</h1>
+                                </tr>
+                            `;
+                    }
+                    
+                    
+                })
+
+
+            }else if(document.location.search == `?user=${login.login.ziad.username}`){
+                console.log("Ziad");
+            }else{
+                document.location.href = "/index.html";
+            }
+        }
+    );
+
+/*End Home Page Script*/
---
+++ loginscript.js
@@ -0,0 +1,47 @@
+/*Start Login Page Script*/
+    const loginBtn = document.getElementById("login-btn");//Get login button
+    loginBtn.addEventListener("click", ()=>{//onclick for button even€€€t
+        login();
+    });
+
+    const user = document.getElementById("username");
+    const pass = document.getElementById("password");
+
+    user.addEventListener("keyup", (e)=>{
+        if (e.code === 'Enter'){
+            e.preventDefault();
+            pass.focus();
+        }
+    })
+
+    pass.addEventListener("keyup", (e)=>{
+        if (e.code === 'Enter'){
+            e.preventDefault();
+            loginBtn.click();
+        }
+    })
+
+
+    function login(){
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+
+        fetch("./json/login.json")
+        .then((response) => {
+        return response.json();
+        })
+        .then( (login)=> {
+                if(username == login.login.osama.username && password == login.login.osama.password){
+                    //Go to home page with user is Osama
+                    document.location.href = "/home.html?user=" + username;
+                }else if(username == login.login.ziad.username && password == login.login.ziad.password){
+                    //Go to home page with user is Ziad
+                    document.location.href = "/home.html?user=" + username;
+                }else{
+                    //Refresh the page
+                    document.getElementById("al").innerHTML = "<h1 class='alert alert-danger mt-5 fs-1' id='alert'>Username Or Password Are Not Correct</h1>";
+                }
+            }
+        );
+    }
+/*End Login Page Script*/
